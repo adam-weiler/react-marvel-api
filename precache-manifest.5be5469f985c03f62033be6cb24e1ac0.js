b57afbad165f56860fbd3bba648dcddb4b4de21f
adam-weiler <adam.weiler@yahoo.ca>
2019-09-04 15:18:35 -0400

    Updates

---
+++ precache-manifest.5be5469f985c03f62033be6cb24e1ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06191e896cd835f1fcf8949da33ee0cb",
+    "url": "/adam-weiler/react-marvel-api/index.html"
+  },
+  {
+    "revision": "fcac1d28f9f7a9e231b7",
+    "url": "/adam-weiler/react-marvel-api/static/css/main.961b9a11.chunk.css"
+  },
+  {
+    "revision": "3eb85da9cdb6118f34cd",
+    "url": "/adam-weiler/react-marvel-api/static/js/2.2247c4e7.chunk.js"
+  },
+  {
+    "revision": "fcac1d28f9f7a9e231b7",
+    "url": "/adam-weiler/react-marvel-api/static/js/main.f8c6a770.chunk.js"
+  },
+  {
+    "revision": "3ec91a857bfa0f90eb0e",
+    "url": "/adam-weiler/react-marvel-api/static/js/runtime~main.0031fe19.js"
+  }
+]);
